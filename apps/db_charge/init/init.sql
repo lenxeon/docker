b201c013e1c748454462ae5778ffab6d3f5c89da
lenxeon <lenxeon@163.com>
2019-07-29 06:19:17 +0800

    gg

--- apps/db_charge/init/init.sql
+++ apps/db_charge/init/init.sql
@@ -1,6 +1,6 @@
 use mysql;
 ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'Sample09X#';
 flush privileges;
-DROP DATABASE IF EXISTS db_charge
+DROP DATABASE IF EXISTS db_charge;
 CREATE DATABASE `db_project` DEFAULT CHARACTER SET utf8mb4 ;
-use db_charge
+use db_charge;
