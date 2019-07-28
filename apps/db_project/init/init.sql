fcbbd5ca163c819c377ddc20d5d1fc938f273812
lenxeon <lenxeon@163.com>
2019-07-29 06:13:03 +0800

    go

--- apps/db_project/init/init.sql
+++ apps/db_project/init/init.sql
@@ -1,1 +1,6 @@
-CREATE DATABASE IF NOT EXISTS db_project DEFAULT CHARSET utf8 COLLATE utf8mb4_general_ci;
+use mysql;
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'Sample09X#';
+flush privileges;
+DROP DATABASE IF EXISTS db_project;
+CREATE DATABASE `db_project` DEFAULT CHARACTER SET utf8mb4 ;
+use db_project;
