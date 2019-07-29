fda04620f1b9d8ba3368f9d9855c290106c05140
lenxeon <lenxeon@163.com>
2019-07-29 22:50:40 +0800

    go

--- apps/tomcat-node/init/init.sh
+++ apps/tomcat-node/init/init.sh
@@ -1,7 +1,4 @@
-base=/usr/local/server/tomcat7/
-killall -9 java
-rm -r -f $base/webapps/
-cd $base
-wget https://static-youth.teamillion.com/soft/apps/apps-web.war
-cp apps-web.war $base/webapps/
-$base/bin/catalina.sh run
+base=/app/tomcat7
+rm -r -f $base/webapps/*
+wget https://static-youth.teamillion.com/soft/apps/apps-web.war?t=`date -d "$currentTime" +%s` -O ./apps-web.war
+cp ./apps-web.war $base/webapps/
