ec5c99cffcbde6669cbb7f7cab30e9fb75b5f15a
lenxeon <lenxeon@163.com>
2019-07-29 20:13:02 +0800

    gg

--- apps/tomcat-node/init/init.sh
+++ apps/tomcat-node/init/init.sh
@@ -1,6 +1,7 @@
-base = /usr/local/server/tomcat7/
-rm -r -f `$base`/webapps/
-cd $base \ 
-&& wget https://static-youth.teamillion.com/soft/apps/apps-web.war \
-&& cp apps-web.war `$base`/webapps/ \
-&& `$base`/bin/catalina.sh run
+base=/usr/local/server/tomcat7/
+killall -9 java
+rm -r -f $base/webapps/
+cd $base
+wget https://static-youth.teamillion.com/soft/apps/apps-web.war
+cp apps-web.war $base/webapps/
+$base/bin/catalina.sh run
