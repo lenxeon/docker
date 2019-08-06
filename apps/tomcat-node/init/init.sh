705c6aa3f3764e69f6cf82fe00cef8a524d2d584
lenxeon <lenxeon@163.com>
2019-08-07 07:21:15 +0800

    gg

--- apps/tomcat-node/init/init.sh
+++ apps/tomcat-node/init/init.sh
@@ -1,4 +1,4 @@
 base=/app/tomcat7
 rm -r -f $base/webapps/*
-wget https://static-youth.teamillion.com/soft/apps/apps-web.war?t=`date -d "$currentTime" +%s` -O ./apps-web.war
+wget http://static-youth.teamillion.com/soft/apps/apps-web.war?t=`date -d "$currentTime" +%s` -O ./apps-web.war
 cp ./apps-web.war $base/webapps/
