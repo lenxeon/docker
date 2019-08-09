40157e139760c46565520561466d4658e4a45d24
lenxeon <lenxeon@163.com>
2019-08-09 21:07:44 +0800

    gg

--- apps/tomcat-node/init/init.sh
+++ apps/tomcat-node/init/init.sh
@@ -1,4 +1,6 @@
 base=/app/tomcat7
+currentTime=`date "+%Y-%m-%d %H:%M:%S"`
+echo $currentTime
 rm -r -f $base/webapps/*
 wget http://static-youth.teamillion.com/soft/apps/apps-web.war?t=`date -d "$currentTime" +%s` -O ./apps-web.war
 cp ./apps-web.war $base/webapps/
