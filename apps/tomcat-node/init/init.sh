a0dffb63d7938151e37ff236b6ccedff26d833b8
lenxeon <lenxeon@163.com>
2019-08-09 22:49:17 +0800

    go

--- apps/tomcat-node/init/init.sh
+++ apps/tomcat-node/init/init.sh
@@ -3,4 +3,4 @@ currentTime=`date "+%Y-%m-%d %H:%M:%S"`
 echo $currentTime
 rm -r -f $base/webapps/*
 wget http://static-youth.teamillion.com/soft/apps/apps-web.war?t=`date -d "$currentTime" +%s` -O ./apps-web.war
-cp ./apps-web.war $base/webapps/
+cp /init/apps-web.war $base/webapps/
