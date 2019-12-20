0a97fed1c8180dace10c15e85a461d40c4fd7546
lenxeon <lenxeon@163.com>
2019-12-21 03:21:21 +0800

    11

--- apps/build.sh
+++ apps/build.sh
@@ -1,10 +1,17 @@
 
 
 
-rm /jdk1.8.0_121.tar
-wget -P / --no-check-certificate https://static-youth.teamillion.com/soft/sun-jdk/jdk1.8.0_121.tar
-rm /apache-tomcat-7.0.94.tar.gz
-wget -P / --no-check-certificate https://static-youth.teamillion.com/soft/apache/apache-tomcat-7.0.94.tar.gz
+rm jdk1.8.0_121.tar
+rm apache-tomcat-7.0.94.tar.gz
+
+git reset --hard
+
+git clean -d -f
+
+git pull
+
+wget  --no-check-certificate https://static-youth.teamillion.com/soft/sun-jdk/jdk1.8.0_121.tar
+wget  --no-check-certificate https://static-youth.teamillion.com/soft/apache/apache-tomcat-7.0.94.tar.gz
 
 
 # 停止服务
@@ -15,12 +22,6 @@ docker ps -a | awk -F ' ' '{print $1}' | xargs docker rm
 
 #删除错误的镜像
 docker images | grep none  | awk -F ' ' '{print $3}' | xargs docker rmi
-
-git reset --hard
-
-git clean -d -f
-
-git pull
 
 docker build -t apps/base centos-base
 
