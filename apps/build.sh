a5900ae065c36fadc7f228a71505fe34a1850aba
lenxeon <lenxeon@163.com>
2019-12-21 05:08:43 +0800

    1122

--- apps/build.sh
+++ apps/build.sh
@@ -4,6 +4,7 @@ currentTime=`date "+%Y-%m-%d %H:%M:%S"`
 rm jdk1.8.0_121.tar
 rm apache-tomcat-7.0.94.tar
 rm apps-web.war
+rm canal.deployer.tar
 
 git reset --hard
 
@@ -14,6 +15,8 @@ git pull
 wget https://static-youth.teamillion.com/soft/sun-jdk/jdk1.8.0_121.tar?t=`date -d "$currentTime" +%s`  -O ./jdk1.8.0_121.tar
 wget https://static-youth.teamillion.com/soft/apache/apache-tomcat-7.0.94.tar?t=`date -d "$currentTime" +%s`  -O ./apache-tomcat-7.0.94.tar
 wget http://static-youth.teamillion.com/soft/apps/apps-web.war?t=`date -d "$currentTime" +%s` -O ./apps-web.war
+wget https://static-youth.teamillion.com/soft/alibaba/canal.deployer.tar?t=`date -d "$currentTime" +%s` -O ./canal.deployer.tar
+
 
 # 停止服务
 docker ps -a | awk -F ' ' '{print $1}' | xargs docker stop 
