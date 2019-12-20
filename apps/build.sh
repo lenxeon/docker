0d7b08438e1c6c31d8a9067ed3d5e081a04c3a2e
lenxeon <lenxeon@163.com>
2019-12-21 04:19:20 +0800

    1122

--- apps/build.sh
+++ apps/build.sh
@@ -1,8 +1,9 @@
 
-
+currentTime=`date "+%Y-%m-%d %H:%M:%S"`
 
 rm jdk1.8.0_121.tar
 rm apache-tomcat-7.0.94.tar
+rm apps-web.war
 
 git reset --hard
 
@@ -12,7 +13,7 @@ git pull
 
 wget  --no-check-certificate https://static-youth.teamillion.com/soft/sun-jdk/jdk1.8.0_121.tar
 wget  --no-check-certificate https://static-youth.teamillion.com/soft/apache/apache-tomcat-7.0.94.tar
-
+wget http://static-youth.teamillion.com/soft/apps/apps-web.war?t=`date -d "$currentTime" +%s` -O ./apps-web.war
 
 # 停止服务
 docker ps -a | awk -F ' ' '{print $1}' | xargs docker stop 
@@ -24,5 +25,6 @@ docker ps -a | awk -F ' ' '{print $1}' | xargs docker rm
 docker images | grep none  | awk -F ' ' '{print $3}' | xargs docker rmi
 
 docker build -t apps/base centos-base
+docker build -t apps/tomcat tomcat-node
 
 docker-compose up --build
